bb966088f2cb4baf46f548b971355a23d5de6e90
bent10 <bent10@stilearning.com>
2023-10-15 12:04:13 +0700

    revert: build for `esm`, `cjs`, and `umd`

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,23 @@
 /// <reference types="vitest" />
 import { resolve } from 'node:path'
-import { defineConfig, type BuildOptions } from 'vite'
+import { defineConfig } from 'vite'
 
-export default defineConfig(({ command, ssrBuild }) => {
-  const isBuildLib = command === 'build' && !ssrBuild
-  const build: BuildOptions = {}
-
-  if (isBuildLib) {
-    Object.assign(build, <BuildOptions>{
-      emptyOutDir: false,
-      lib: {
-        entry: resolve(__dirname, 'src/index.ts'),
-        name: 'myLib',
-        formats: ['umd'],
-        fileName: 'index'
-      }
-      // rollupOptions: {
-      //   external: ['vue'],
-      //   output: { globals: { vue: 'Vue' } }
-      // }
-    })
-  }
-
-  return {
-    build,
-    test: {
-      globals: true,
-      include: ['test/*.test.ts']
+export default defineConfig({
+  build: {
+    minify: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'myLib',
+      formats: ['es', 'cjs', 'umd'],
+      fileName: 'index'
     }
+    // rollupOptions: {
+    //   external: [],
+    //   output: { globals: { } }
+    // }
+  },
+  test: {
+    globals: true,
+    include: ['test/*.test.ts']
   }
 })
